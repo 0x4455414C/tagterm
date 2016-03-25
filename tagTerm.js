2213534577860a3017f290d9152c30c3613bf607
dual1tyx <dual1tyx@hotmail.co.uk>
2016-03-25 21:02:34 +0000

    updated method

--- tagTerm.js
+++ tagTerm.js
@@ -1,4 +1,4 @@
 module.exports = tagterm
-function updateString(input, term, tag) {
+function tagterm(input, term, tag) {
     return input.replace(new RegExp('(^|\)('+term+')(\|$)','ig'), '$1<'+tag+'>$2</'+tag+'>$3');
 }
